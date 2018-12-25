dc91ad9ae3bf5768e2204f051d57c505db53459b
Aleksander Spro <abspro@gmail.com>
2018-12-25 13:49:53 +0100

    Basic implementation

--- src/Fable.C3/Fable.C3.fs
+++ src/Fable.C3/Fable.C3.fs
@@ -7,4 +7,30 @@ open Fable.Import.Browser
 let [<Import("*","C3")>] c3 : IExports = jsNative
 
 type [<AllowNullLiteral>] IExports =
-    abstract generate: config: unit -> unit
+  abstract generate: config: unit -> unit
+
+type ArrayOrString =
+  U2<ResizeArray<string>, string>
+
+type [<AllowNullLiteral>] TargetIds =
+  abstract ids: ArrayOrString with get, set
+
+type PrimitiveArray =
+  Array<U3<string, bool, float> option>
+
+type [<AllowNullLiteral>] Data =
+  abstract columns: ResizeArray<PrimitiveArray> option with get, set
+  abstract ``type``: string option with get, set
+
+type [<AllowNullLiteral>] ChartConfiguration =
+  abstract bindto: U2<string, HTMLElement> option with get, set
+  abstract data: Data with get, set
+
+type [<AllowNullLiteral>] ChartAPILoadArgs =
+  abstract columns: ResizeArray<PrimitiveArray> option with get, set
+
+
+type [<AllowNullLiteral>] ChartAPI =
+  abstract load: args: ChartAPILoadArgs -> unit
+  abstract unload: ?targetIds: TargetIds * ?``done``: (unit -> obj option) -> obj option
+  abstract destroy: unit -> unit
